df9e8927cc3594c6c962e02d11c695a26e21b310
jose.nilton <jose.nilton@ifood.com.br>
2024-09-29 10:42:58 -0300

    Ajustes

--- app/app.py
+++ app/app.py
@@ -13,27 +13,53 @@ get_config()
 get_widgets()
 
 # BASE
-get_data()
+# get_data()
 
 # SIDEBAR COM DURACAO
-get_duration()
+# get_duration()
 
 # FILTROS
-try:
-    if st.session_state.atualizar or (len(st.session_state['dataframe_base']) > 0 and st.session_state.categorias in list(set(st.session_state['dataframe_base'][0]['categoria']))):
+# try:
+#     if st.session_state.atualizar or (len(st.session_state['dataframe_base']) > 0 and st.session_state.categorias in list(set(st.session_state['dataframe_base'][0]['categoria']))):
 
-        df_base_st = pd.DataFrame(st.session_state['dataframe_base'][0])
+#         df_base_st = pd.DataFrame(st.session_state['dataframe_base'][0])
 
-        get_filters(df_base_st)
+#         get_filters(df_base_st)
 
-        df_filtered = apply_filters(df_base_st)
+#         df_filtered = apply_filters(df_base_st)
 
-        get_final_dataframe(df_base_st_filt = df_filtered)
+#         get_final_dataframe(df_base_st_filt = df_filtered)
 
+#     else:
+#         st.write('Selecione uma categoria!')
+# except:
+#     pass
+
+# if st.session_state.limpar_cache:
+#     st.cache_data.clear()
+
+if st.session_state.atualizar:
+    # DADOS
+    inicio = datetime.datetime.now(tz = pytz.timezone('America/Sao_Paulo')).replace(microsecond = 0, tzinfo = None)
+    st.session_state['lista_inicio'].insert(0, inicio) 
+
+    if st.session_state.categorias == 'series':
+        # dados = asyncio.run(GetSeriesMetadata(st.session_state.categorias).get_series_metadata())
+        dados = GetSeriesMetadata(st.session_state.categorias).get_series_metadata_sync()
+        # dados = await GetSeriesMetadata(st.session_state.categorias).get_series_metadata()
     else:
-        st.write('Selecione uma categoria!')
-except:
-    pass
+        # dados = asyncio.run(GetMovieMetadata(st.session_state.categorias).get_movie_metadata())
+        dados = GetMovieMetadata(st.session_state.categorias).get_movie_metadata_sync()
+        # dados = await GetMovieMetadata(st.session_state.categorias).get_movie_metadata()
 
-if st.session_state.limpar_cache:
-    st.cache_data.clear()
+    fim = datetime.datetime.now(tz = pytz.timezone('America/Sao_Paulo')).replace(microsecond = 0, tzinfo = None)
+    st.session_state['lista_fim'].insert(0, fim) 
+
+    duracao = fim - inicio
+    st.session_state['lista_duracao'].insert(0, duracao)
+
+    # df_base = pd.DataFrame(dados[0])
+    df_base = pd.DataFrame(dados)
+    st.session_state['dataframe_base'].insert(0, df_base.to_dict(orient = 'list'))
+
+dados
